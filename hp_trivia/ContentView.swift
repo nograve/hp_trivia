f74c05153e26c1355f70f5956573cdca4cc66083
Ihor Zakharov <ihor.zakharov@cleveroad.com>
2024-01-25 13:04:04 +0200

    Added ZStack with GeometryReader

--- hp_trivia/ContentView.swift
+++ hp_trivia/ContentView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            ZStack {
+                
+            }.frame(width: geo.size.width, height: geo.size.height)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 

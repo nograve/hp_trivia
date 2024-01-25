5ee254a46bd4319410ed63a0ddc2d1c371428e61
Ihor Zakharov <ihor.zakharov@cleveroad.com>
2024-01-25 12:53:26 +0200

    Initial Commit

---
+++ hp_trivia/hp_triviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  hp_triviaApp.swift
+//  hp_trivia
+//
+//  Created by Ihor Zakharov on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct hp_triviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
